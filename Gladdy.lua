13a26d82c60b7c33339ba957fff025a8abf658d5
Sumsebrum <kyle@xicon.de>
2022-03-27 21:04:47 +0200

    bump version + readme changelog

--- Gladdy.lua
+++ Gladdy.lua
@@ -27,11 +27,11 @@ local LibStub = LibStub
 
 ---------------------------
 
-local MAJOR, MINOR = "Gladdy", 6
+local MAJOR, MINOR = "Gladdy", 7
 local Gladdy = LibStub:NewLibrary(MAJOR, MINOR)
 local L
 Gladdy.version_major_num = 2
-Gladdy.version_minor_num = 0.10
+Gladdy.version_minor_num = 0.11
 Gladdy.version_num = Gladdy.version_major_num + Gladdy.version_minor_num
 Gladdy.version_releaseType = RELEASE_TYPES.release
 Gladdy.version = PREFIX .. string.format("%.2f", Gladdy.version_num) .. "-" .. Gladdy.version_releaseType
