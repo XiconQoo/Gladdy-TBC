f1652e2b42b448d59d37ecb30f14cb3c4d38fdf7
Sumsebrum <j.schaefer@xicon.de>
2022-07-29 07:45:46 +0200

    add DRList-1.0 as lib

--- Modules/Diminishings.lua
+++ Modules/Diminishings.lua
@@ -7,7 +7,8 @@ local CreateFrame = CreateFrame
 local GetTime = GetTime
 
 local Gladdy = LibStub("Gladdy")
-local DRData = LibStub("DRData-1.0-BCC")
+--local DRData = LibStub("DRData-1.0-BCC")
+local DRData = LibStub("DRList-1.0")
 local L = Gladdy.L
 local function defaultCategories()
     local categories = {}
