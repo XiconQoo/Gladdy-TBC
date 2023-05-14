76b9a414e5b9b8236135de5aa175874957bc7be3
Sumsebrum <kyle@xicon.de>
2023-05-14 21:06:20 +0200

    GladdySearchEditBox docs

--- Libs/AceGUI-3.0-GladdySearchEditBox/GladdySearchEditBox-Library.lua
+++ Libs/AceGUI-3.0-GladdySearchEditBox/GladdySearchEditBox-Library.lua
@@ -13,3 +13,53 @@ end
 function lib:Register (typename, options)
 	AceGUI:RegisterWidgetType ("GladdySearchEditBox"..typename, function() return Constructor(options) end, MINOR)
 end
+
+
+
+--- Example on how to use the Edit GladdySearchEditBox
+
+--- init a Predictor
+--[[
+local Predictor = {}
+function Predictor:Initialize()
+end
+function Predictor:GetValues(input)
+    local values = {}
+    local spellName, icon = GetSpellInfo(input)
+    if (spellName) then
+    	values[input] = {
+			text = spellName .. " - (" .. input .. ")",
+			icon = icon
+		}
+	end
+    return values
+end
+function Predictor:GetValue(text, key)
+    return key
+end
+function Predictor:GetHyperlink(key)
+    return "spell:" .. key .. ":0"
+end
+]]
+
+--- Register as follows
+--[[
+LibStub("AceGUI-3.0-GladdySearchEditBox"):Register("Auras", Predictor)
+]]
+
+--- usage in config
+--[[
+editBox = {
+	order = 1,
+	width = "2",
+	name = L["Add Aura"],
+	type = "input",
+	dialogControl = "GladdySearchEditBoxAuras",
+	get = function()
+
+	end,
+	set = function(_, value)
+
+	end,
+},
+]]
