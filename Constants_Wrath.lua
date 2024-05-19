37ea37220598515e21bbbe86a3305ff674ba9597
Sumsebrum <j.schaefer@xicon.de>
2024-05-19 02:26:38 +0200

    clean up totem data

--- Constants_Wrath.lua
+++ Constants_Wrath.lua
@@ -1253,11 +1253,15 @@ end
 
 local totemData = {
     -- Fire
+    [string_lower("Fire Elemental Totem")] = {id = 32982,texture = select(3, GetSpellInfo(32982)), color = {r = 0, g = 0, b = 0, a = 1}},
+    [string_lower("Totem of Wrath")] = {id = 30706,texture = select(3, GetSpellInfo(30706)), color = {r = 0, g = 0, b = 0, a = 1}},
     -- Water
-    --[string_lower("Disease Cleansing Totem")] = {id = 8170,texture = select(3, GetSpellInfo(8170)), color = {r = 0, g = 0, b = 0, a = 1}, pulse = 3},
+    [string_lower("Disease Cleansing Totem")] = {id = 8170,texture = select(3, GetSpellInfo(8170)), color = {r = 0, g = 0, b = 0, a = 1}, pulse = 3},
     [string_lower("Mana Spring Totem")] = { id = 5675, texture = select(3, GetSpellInfo(5675)), color = { r = 0, g = 0, b = 0, a = 1 } },
     -- Earth
+    [string_lower("Earth Elemental Totem")] = {id = 33663,texture = select(3, GetSpellInfo(33663)), color = {r = 0, g = 0, b = 0, a = 1}},
     -- Air
+    [string_lower("Wrath of Air Totem")] = {id = 3738,texture = select(3, GetSpellInfo(3738)), color = {r = 0, g = 0, b = 0, a = 1}},
 }
 
 local totemSpellIdToPulse = {
@@ -1266,6 +1270,26 @@ local totemSpellIdToPulse = {
 }
 
 local totemNpcIdsToTotemData = {
+    --Fire
+    [17539] = totemData[string_lower("Totem of Wrath")],
+    [22970] = totemData[string_lower("Totem of Wrath")],
+    [22971] = totemData[string_lower("Totem of Wrath")],
+    [30652] = totemData[string_lower("Totem of Wrath")],
+    [30653] = totemData[string_lower("Totem of Wrath")],
+    [30654] = totemData[string_lower("Totem of Wrath")],
+
+    [15430] = totemData[string_lower("Earth Elemental Totem")],
+    [24649] = totemData[string_lower("Earth Elemental Totem")],
+    [39387] = totemData[string_lower("Earth Elemental Totem")],
+    [40247] = totemData[string_lower("Earth Elemental Totem")],
+    [72307] = totemData[string_lower("Earth Elemental Totem")],
+
+    [15439] = totemData[string_lower("Fire Elemental Totem")],
+    [40830] = totemData[string_lower("Fire Elemental Totem")],
+    [41337] = totemData[string_lower("Fire Elemental Totem")],
+    [41346] = totemData[string_lower("Fire Elemental Totem")],
+    [72301] = totemData[string_lower("Fire Elemental Totem")],
+
     [5924] = totemData[string_lower("Disease Cleansing Totem")],
 
     [3573] = totemData[string_lower("Mana Spring Totem")],
@@ -1277,6 +1301,9 @@ local totemNpcIdsToTotemData = {
     [31186] = totemData[string_lower("Mana Spring Totem")],
     [31189] = totemData[string_lower("Mana Spring Totem")],
     [31190] = totemData[string_lower("Mana Spring Totem")],
+
+    [15447] = totemData[string_lower("Wrath of Air Totem")],
+    [36556] = totemData[string_lower("Wrath of Air Totem")],
 }
 
 local totemDataShared, totemNpcIdsToTotemDataShared, totemSpellIdToPulseShared = Gladdy:GetSharedTotemData()
