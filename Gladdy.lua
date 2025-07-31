566c6f143a228448d12a22a45d68c47cb0ee8b12
Sumsebrum <kyle@xicon.de>
2025-07-31 03:48:02 +0200

    v2.52-Release

--- Gladdy.lua
+++ Gladdy.lua
@@ -29,11 +29,11 @@ local GetSpellInfo = GetSpellInfo
 
 ---------------------------
 
-local MAJOR, MINOR = "Gladdy", 20
+local MAJOR, MINOR = "Gladdy", 21
 local Gladdy = LibStub:NewLibrary(MAJOR, MINOR)
 local L
 Gladdy.version_major_num = 2
-Gladdy.version_minor_num = 0.51
+Gladdy.version_minor_num = 0.52
 Gladdy.version_num = Gladdy.version_major_num + Gladdy.version_minor_num
 Gladdy.version_releaseType = RELEASE_TYPES.release
 Gladdy.version = PREFIX .. string.format("%.2f", Gladdy.version_num) .. "-" .. Gladdy.version_releaseType
