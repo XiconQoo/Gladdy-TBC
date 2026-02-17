98ad771648280b30f6c658a67dfb88ca749a555c
Sumsebrum <j.schaefer@xicon.de>
2026-02-17 06:57:06 +0100

    2.64-Beta

--- Gladdy.lua
+++ Gladdy.lua
@@ -33,7 +33,7 @@ local MAJOR, MINOR = "Gladdy", 26
 local Gladdy = LibStub:NewLibrary(MAJOR, MINOR)
 local L
 Gladdy.version_major_num = 2
-Gladdy.version_minor_num = 63
+Gladdy.version_minor_num = 64
 Gladdy.version_num = Gladdy.version_major_num + Gladdy.version_minor_num
 Gladdy.version_releaseType = RELEASE_TYPES.beta
 Gladdy.version = PREFIX .. string.format("%.2f", Gladdy.version_num) .. "-" .. Gladdy.version_releaseType
